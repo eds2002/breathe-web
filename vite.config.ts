7be77fcc59b589d39178b202f6ff6b85ffc135f2
Eduardo Sanchez <es23jr@gmail.com>
2025-07-07 17:07:22 -0400

    whoops

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/breathe-web/',
+
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
