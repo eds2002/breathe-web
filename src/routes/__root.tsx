b6b31c940ee5c65906de9daf0308f17f196b9abd
Eduardo Sanchez <es23jr@gmail.com>
2025-07-14 11:14:41 -0400

    Add twitter head.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,42 @@
 import { Header } from "@/components/header";
-import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{
+				name: "twitter:card",
+				content: "summary",
+			},
+			{
+				name: "twitter:image",
+				content: "/logo512.png",
+			},
+			{
+				name: "twitter:title",
+				content: "Oak",
+			},
+			{
+				name: "twitter:description",
+				content: "Oak - Collectable Breathing Exercises",
+			},
+			{
+				property: "og:type",
+				content: "website",
+			},
+			{
+				property: "og:image",
+				content: "/logo512.png",
+			},
+			{
+				property: "og:title",
+				content: "Oak",
+			},
+		],
+	}),
 	component: () => (
 		<>
-			<HeadContent />
 			<Header />
 			<Outlet />
 			<TanStackRouterDevtools />
